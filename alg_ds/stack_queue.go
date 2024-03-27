6037f68bd734e4da162c1ba3b21f9955c213b128
HZreal <huangzhen_happy@163.com>
2024-03-27 13:55:23 +0800

    stack queue

---
+++ alg_ds/stack_queue.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 栈：先进后出，先进队的数据最后才出来。在英文的意思里，stack 可以作为一叠的意思，这个排列是垂直的，你将一张纸放在另外一张纸上面，先放的纸肯定是最后才会被拿走，因为上面有一张纸挡住了它。
+// 队列：先进先出，先进队的数据先出来。在英文的意思里，queue 和现实世界的排队意思一样，这个排列是水平的，先排先得。
+
+// 可以用数据结构：链表（可连续或不连续的将数据与数据关联起来的结构），或 数组（连续的内存空间，按索引取值） 来实现 栈（stack） 和 队列 (queue)。
+// 		数组实现：能快速随机访问存储的元素，通过下标 index 访问，支持随机访问，查询速度快，但存在元素在数组空间中大量移动的操作，增删效率低。
+// 		链表实现：只支持顺序访问，在某些遍历操作中查询速度慢，但增删元素快。
+
+// 数组栈，后进先出
+type ArrayStack struct {
+	array []string   // 底层切片
+	size  int        // 栈的元素数量
+	lock  sync.Mutex // 为了并发安全使用的锁
+}
+
+func (stack *ArrayStack) Pop() string {
+	stack.lock.Lock()
+	defer stack.lock.Unlock()
+
+	// 栈中元素已空
+	if stack.size == 0 {
+		panic("empty")
+	}
+
+	// 栈顶元素
+	v := stack.array[stack.size-1]
+
+	// 切片收缩，但可能占用空间越来越大
+	//stack.array = stack.array[0 : stack.size-1]
+
+	// 创建新的数组，空间占用不会越来越大，但可能移动元素次数过多
+	newArray := make([]string, stack.size-1, stack.size-1)
+	for i := 0; i < stack.size-1; i++ {
+		newArray[i] = stack.array[i]
+	}
+	stack.array = newArray
+
+	// 栈中元素数量-1
+	stack.size = stack.size - 1
+	return v
+}
+
+// 入栈
+func (stack *ArrayStack) Push(v string) {
+	stack.lock.Lock()
+	defer stack.lock.Unlock()
+
+	// 放入切片中，后进的元素放在数组最后面
+	stack.array = append(stack.array, v)
+
+	// 栈中元素数量+1
+	stack.size = stack.size + 1
+}
+
+// 获取栈顶元素
+func (stack *ArrayStack) Peek() string {
+	// 栈中元素已空
+	if stack.size == 0 {
+		panic("empty")
+	}
+
+	// 栈顶元素值
+	v := stack.array[stack.size-1]
+	return v
+}
+
+// 栈大小
+func (stack *ArrayStack) Size() int {
+	return stack.size
+}
+
+// 栈是否为空
+func (stack *ArrayStack) IsEmpty() bool {
+	return stack.size == 0
+}
+
+func main() {
+	arrayStack := new(ArrayStack)
+	arrayStack.Push("cat")
+	arrayStack.Push("dog")
+	arrayStack.Push("hen")
+	fmt.Println("size:", arrayStack.Size())
+	fmt.Println("pop:", arrayStack.Pop())
+	fmt.Println("pop:", arrayStack.Pop())
+	fmt.Println("size:", arrayStack.Size())
+	arrayStack.Push("drag")
+	fmt.Println("pop:", arrayStack.Pop())
+}
