652bd5ac8dcaac208752bdd8e9c9388d9926564a
HZreal <hzreal0823@gmail.com>
2024-08-06 12:28:32 +0800

    feat: GMP model

---
+++ concurrence/GMPmodel.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+ * @Author huang
+ * @Date 2024-06-31
+ * @File: GMPModel.go
+ * @Description:
+ */
+
+/**
+ * GMP 模型
+ * 参考：https://learnku.com/articles/41728
+ */
+
+func main() {
+	// G（协程）
+	// M（系统线程）
+	// P（处理器）
+
+	// P 本地队列、P 列表
+
+	// 多线程模型
+
+	// P、M 数量问题
+	// P、M 何时被创建
+	// 调度器的设计策略
+	// 特殊的 M0 和 G0
+	// 协程调度运行机制
+}
